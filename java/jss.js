ee9a343d9ef4d7b9d4f369dca3ce8df2dd2ce9b7
josecastilo-84 <77947918+josecastilo-84@users.noreply.github.com>
2021-01-28 23:49:17 -0500

    Add files via upload
    RECUPERACION codigo con alertas

---
+++ java/jss.js
@@ -0,0 +1,28 @@
+function validaciondecampos() {
+
+    var nombre=document.getElementById("name").value;    
+    var tipom=document.getElementById("type").value;    
+
+
+    /*if(tipom ==""){
+        alert("el campo debe estar vacio")
+        document.getElementById("name").focus();
+        }
+
+    if(nombre ==""){
+    alert("el campo debe estar vacio")
+    document.getElementById("name").focus();
+    }*/
+
+    var mensaje="";
+    if(tipo "=="|| nombre =="){
+        if(type==){
+            mensaje += "El campo Tipo de mensaje esta vacio";            
+        }
+        if(nombre"=="){            
+            mensaje += "\El campo Tipo de mensaje esta vacio";
+            alert (mensaje);
+         } 
+    }
+}
+
